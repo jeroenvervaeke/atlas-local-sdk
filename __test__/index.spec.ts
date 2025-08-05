e76e046ef686b6c43d769d1ff09cef7016f3257e
Jeroen Vervaeke <jeroenvervaeke@users.noreply.github.com>
2025-08-05 12:41:59 +0100

    Added edge cases for macos runners

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -2,13 +2,28 @@ import test from 'ava'
 
 import { Client } from '../index'
 
-test('smoke test, list deployments returns nothing', async (t) => {
-  // Create client
-  let client = await Client.connect();
+test('smoke test: list deployments', async (t) => {
+  let client: Client | null = null;
+
+  try {
+    // Create client
+    client = await Client.connect();
+
+  }
+  catch (e: any) {
+    // If docker is not running we get this error
+    // any other error means failure
+    t.is(e?.message, 'connect to docker');
+    return;
+  }
+
+  if (client == null) {
+    t.fail('Client not created');
+    return;
+  }
 
   // List deployments
-  let deployments = await client.listDeployments();
-
-  // Verify no deployments are returned
-  t.is(deployments.length, 0);
+  // We don't care about the number, we're just testing that the method doesn't fail
+  await client.listDeployments();
+  t.pass();
 })
